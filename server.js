67509b77dd3f78d591501d8ad7aa50eaf3ec3536
Mikhail Serebriakov <mishacyb@gmail.com>
2020-05-30 19:13:32 +0000

    Removed /api from all routes

--- server.js
+++ server.js
@@ -38,17 +38,17 @@ const itemsRoutes = require("./routes/items");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/items", itemsRoutes(db));
+app.use("/users", usersRoutes(db));
+app.use("/items", itemsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
-// app.get("/", (req, res) => {
-//   res.render("index");
-// });
+app.get("/", (req, res) => {
+  res.redirect("/items");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
