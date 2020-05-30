9f238790061b96bafbd28f458e77b4a5af28aebd
Mikhail Serebriakov <mishacyb@gmail.com>
2020-05-30 01:19:57 +0000

    Added seeds for users and items

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,12 @@
 -- Users table seeds here (Example)
--- INSERT INTO users (name) VALUES ('Alice');
--- INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, phone_number) VALUES ('Corney	Godon', 'cgodon0@techcrunch.com', '740-496-0681');
+INSERT INTO users (name, email, phone_number) VALUES ('Raychel Bleue', 'rbleue1@smugmug.com', '932-862-2123');
+INSERT INTO users (name, email, phone_number) VALUES ('Dalila	Hofner', 'dhofner2@biglobe.ne.jp', '938-252-4690');
+INSERT INTO users (name, email, phone_number) VALUES ('Ollie Laurisch', 'olaurisch3@sphinn.com', '401-110-8551');
+INSERT INTO users (name, email, phone_number) VALUES ('Darbie	Benninger', 'dbenninger4@discovery.com', '871-620-6144');
+INSERT INTO users (name, email, phone_number) VALUES ('Paquito Abbiss', 'pabbiss5@tinyurl.com', '307-710-8527');
+INSERT INTO users (name, email, phone_number) VALUES ('Wendell Stanlake', 'wstanlake6@webnode.com', '716-546-4098');
+INSERT INTO users (name, email, phone_number) VALUES ('Fernando Lockett', 'fernfern@shaw.ca', '367-897-1679');
+INSERT INTO users (name, email, phone_number) VALUES ('Jerry Larson', 'jerrBear@gmail.com', '345-346-1223');
+INSERT INTO users (name, email, phone_number) VALUES ('Morgan Newhook', 'marganNew123@hotmail.com', '445-453-1888');
+INSERT INTO users (name, email, phone_number) VALUES ('Jacob Saps', 'sapper@gmail.com', '101-234-455');
---
+++ db/seeds/02_items.sql
@@ -0,0 +1,16 @@
+INSERT INTO items (seller_id, name, description, price,  image_url, city)
+VALUES (1, '10pcs Cotton Washable/Reusable face mask', 'Quality Antibacterial 100% Organic Cotton, 2 layers of well-knitted cotton', 3000, 'https://images-na.ssl-images-amazon.com/images/I/71uwy9xeyDL._AC_SY355_.jpg', 'Coquitlam');
+INSERT INTO items (seller_id, name, description, price,  image_url, city)
+VALUES (2, 'Premium Hand sanitizer', 'The highest quality hand sanitizer', 1566, 'https://media4.s-nbcnews.com/i/newscms/2020_10/1544876/5042825743-5e5ebad345bc9_7b3f3f1eb2cb4c8ee0bddf91acb40d8d.jpg', 'North Vancouver');
+INSERT INTO items (seller_id, name, description, price,  image_url, city)
+VALUES (3, 'Lysol Disinfecting Wipes', 'Disposable wipes that kill 99.99% of bacteria', 1233, 'https://s7d2.scene7.com/is/image/homedepotcanada/p_1000681684.jpg', 'Richmond');
+INSERT INTO items (seller_id, name, description, price,  image_url, city)
+VALUES (2, 'Hand sanitizer one gallon', 'Brand new sealed hand sanitizer.', 9000, 'https://www.perfectvitaminproducts.com/wp-content/uploads/689264_handSanitizer420-1.png', 'Burnaby');
+INSERT INTO items (seller_id, name, description, price,  image_url, city)
+VALUES (4, 'Fashionable face mask', 'Be a trendsetter with our new fashionable face mask.', 1300, 'https://images.craigslist.org/00303_bEWReFGoRQm_0fZ0fk_600x450.jpg', 'Richmond');
+INSERT INTO items (seller_id, name, description, price,  image_url, city)
+VALUES (5, 'Lysol Disinfecting Wipes(Triple pack)', 'Disposable wipes that kill 99.99% of bacteria', 3000, 'https://i5.walmartimages.com/asr/176a25fb-ce00-4d5a-bfda-0ad4dbe59a77_1.6c4f2bfebdb8298ca3c46089f2ffe14d.jpeg?odnWidth=612&odnHeight=612&odnBg=ffffff', 'Langley');
+INSERT INTO items (seller_id, name, description, price,  image_url, city)
+VALUES (5, 'N95 Face Masks', 'Heavy duty, hospital grade face masks', 2400, 'https://bramptonist.com/wp-content/uploads/2020/03/face-mask-n95-ppe-protection-scaled.jpg', 'Vancouver');
+
+
