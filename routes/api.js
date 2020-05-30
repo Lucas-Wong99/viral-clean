cc07cbea4e96541d8a9727e20c27d9ed58132bac
Mikhail Serebriakov <mishacyb@gmail.com>
2020-05-30 22:00:53 +0000

    Added dynamic username in the header. Created a route for creating a listing and connected it to the view

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-
+const { retrieveUserFromDB } = require('../lib/helpers');
 const itemsRoutes = require("./items");
 
 module.exports = (db) => {
@@ -13,12 +13,6 @@ module.exports = (db) => {
     res.redirect('/');
   });
 
-  // Logout a user
-  router.get('/logout/', (req, res) => {
-    req.session = null;
-    res.redirect('/');
-  });
-
   // Get items for the user with this id
   router.get('/myitems', (req, res) => {
     const queryParams = [req.session.user_id];
@@ -26,11 +20,18 @@ module.exports = (db) => {
     SELECT * FROM items
     WHERE seller_id = $1;
     `;
-    db.query(query, queryParams)
+
+    retrieveUserFromDB(db, req.session.user_id)
+    .then((username) => {
+      db.query(query, queryParams)
       .then(data => {
+        console.log(username);
         const items = data.rows;
-        res.render('my_listings', { items });
+        res.render('my_listings', { items, username });
       });
+    })
+
+
   });
 
   return router;
