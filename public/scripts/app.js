05fcf4a881f29a74bdf09fd5d692302a2c120573
Mikhail Serebriakov <mishacyb@gmail.com>
2020-06-01 23:46:11 +0000

    Refactored the rendering of filtered results, now it renders on the server side

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,112 +1,14 @@
 $(() => {
-  // Creates and appends item elements to the items-container, given an array of items
-  const renderItems = (items) => {
-    $(".items-container").empty();
-    for (const item of items) {
-      let str = `
-        <div class='item-container'>
-          <div class='item-header'>
-            <span>$${item.price / 100}</span>
-            <span>3 days ago</span>
-          </div>
-          <div class='item-main'>
-            <img src="${item.image_url}">
-            <a href="/api/items/${item.id}"><h3>${item.name}</h3></a>
-          </div>
-          <div class='item-footer'>
-            <a class="favourite-button">`;
 
-            if (item.user_id === null) {
-            str += `<i class="fas fa-star" data-id="${item.id}"></i>`;
-            } else {
-              str += `<i class="fas fa-star yellow" data-id="${item.id}"></i>`;
-            }
-            str += `</a>
-            </div>
-            </div>`;
-      $(".items-container").append(str);
-    }
-  };
+  addEventListenerToSearchFormSubmit();
 
-  $("#search-form").submit(function(event){
-    event.preventDefault();
-    const data = $(this).serialize();
-    $.ajax({
-      type: "GET",
-      url: "/api/items/filter",
-      data: data
-    })
-      .done(items => {
-        renderItems(items);
-      })
-  });
+  addEventListenerToSearchFormOnChange();
 
-  $(".search-form-order").on('change', function(event){
-    event.preventDefault();
-    const data = $('#search-form').serialize();
-    $.ajax({
-      type: "GET",
-      url: "/api/items/filter",
-      data: data
-    })
-      .done(items => {
-        renderItems(items);
-      })
-  });
+  addEventListenerToFavouriteButtons();
 
-  $('.favourite-button').on('click', (event) => {
-    console.log(event);
-    if ($(event.target).hasClass('yellow')) {
-        const item_id = event.target.getAttribute("data-id");
-        const data = { item_id };
-        $.ajax({
-          type: "POST",
-          url: `/api/favourites/${item_id}`,
-          data: data
-        })
-        .done(result => {
-          $(event.target).removeClass("yellow");
-          $(`#favourites-container > *[data-id="${result.item_id}"]`).remove();
-        });
-    } else {
-      const data = { item_id: event.target.getAttribute("data-id") };
-      $.ajax({
-        type: "POST",
-        url: "/api/favourites",
-        data: data
-      })
-      .done(confirmed => {
-        $(event.target).addClass("yellow");
-      })
-    }
-  });
+  addEventListenerToDeleteButtons();
 
-  $('.delete-button').on('click', (event) => {
-    const item_id = event.target.getAttribute("data-id");
-        const data = { item_id };
-        $.ajax({
-          type: "POST",
-          url: `/api/items/${item_id}/delete`,
-          data: data
-        })
-        .done(result => {
-          console.log(result);
-          $(`#all-items-container > *[data-id="${result.id}"]`).remove();
-        });
-  });
+  addEventListenerToSellButtons();
 
-  $('.sell-button').on('click', (event) => {
-    const item_id = event.target.getAttribute("data-id");
-        const data = { item_id };
-        $.ajax({
-          type: "POST",
-          url: `/api/items/${item_id}/sell`,
-          data: data
-        })
-        .done(result => {
-          console.log(result);
-          $(`.items-container > *[data-id="${result.id}"] > .item-header > .item-price`).text('SOLD');
-        });
-  });
 });
 
